51b8577dafa5c796d2de3839082caa3141b11896
120-cell <120.cheshire@gmail.com>
2025-11-16 06:26:16 +0100

    initial commit.

---
+++ efficiency_calc.py
@@ -0,0 +1,41 @@
+from tapping_styles import *
+import math
+
+
+def main():
+    style_comparison(four_key_styles + eight_key_styles, 16, 3)
+    
+
+def style_comparison(style_list, max_pattern_length, decimal_places):
+    name_len = max([len(style.__name__) for style in style_list])
+    print('pattern length'.rjust(name_len) + ':\t',
+          '\t'.join([str(i) for i in range(1, max_pattern_length + 1)]))
+    for style in style_list:
+        effs = [average_finger_eff(style, l) for l in range(1, max_pattern_length + 1)]
+        print(f'{style.__name__.rjust(name_len)}:\t',
+               '\t'.join([f'{eff:.{decimal_places}f}' for eff in effs]))
+
+
+def average_finger_eff(tapping_style, pattern_length):
+    efficiencies = []
+    for i in range(2**pattern_length):
+        pattern = [int(b) for b in bin(i)[2:].zfill(pattern_length)]
+        efficiencies.append(finger_efficiency(tapping_style, pattern))
+    average = sum(efficiencies) / 2**pattern_length
+    return average
+
+
+def finger_efficiency(tapping_style, pattern):
+    layout, _ = tapping_style([])
+    reps = 2 * math.ceil(len(layout) / len(pattern)) + 1
+    pattern = pattern * reps
+    _, fingering = tapping_style(pattern)
+    distances = []
+    for finger in range(len(layout)):
+        indices = [i for i, x in enumerate(fingering) if x == finger]
+        distances += [indices[i + 1] - indices[i] for i in range(len(indices) - 1)]
+    return min(distances)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tapping_styles.py
@@ -0,0 +1,136 @@
+
+
+def kddk_full_wrist_alt(pattern, starting_finger=None):
+    layout = [1,0,0,1]
+    if starting_finger is None:
+        if pattern and pattern[0]:
+            starting_finger = 0
+        else:
+            starting_finger = 1
+    elif layout[starting_finger] != pattern[0]:
+        raise ValueError('starting finger does not match pattern color')
+    if starting_finger < 2:
+        wrist = 0
+
+    fingering = [starting_finger]
+    for note in pattern[1:]:
+        wrist = 1 - wrist
+        if note:
+            fingering.append(0 + 3 * wrist)
+        else:
+            fingering.append(1 + 1 * wrist)
+    return layout, fingering
+
+
+def kddk_full_roll(pattern, starting_finger=None):
+    layout = [1,0,0,1]
+    if starting_finger is None:
+        wrist = 0
+    else:
+        if layout[starting_finger] != pattern[0]:
+            raise ValueError('starting finger does not match pattern color')
+        wrist = starting_finger // 2
+    first_note = None
+    
+    fingering = []
+    for note in pattern:
+        if note == first_note:
+            wrist = 1 - wrist
+
+        if note:
+            fingering.append(0 + 3 * wrist)
+        else:
+            fingering.append(1 + 1 * wrist)
+
+        if first_note == None:
+            first_note = note
+        elif note != first_note:
+            wrist = 1 - wrist
+            first_note = None
+    return layout, fingering
+
+
+def global_rotation(layout, rotation, pattern):
+    i = 0
+    fingering = []
+    for note in pattern:
+        while not layout[rotation[i]] == note:
+            i = (i + 1) % len(rotation)
+        fingering.append(rotation[i])
+        i = (i + 1) % len(rotation)
+    return fingering
+
+
+def kddk_half_roll(pattern, starting_finger=0):
+    layout = [1,0,0,1]
+    rotation = [0,1,3,2]
+    if starting_finger < 2:
+        starting_index = starting_finger
+    else:
+        starting_index = 5 - starting_finger
+    rotation = rotation[starting_index:] + rotation[:starting_index]
+    return layout, global_rotation(layout, rotation, pattern)
+
+
+def kdkddkdk_half_roll(pattern, starting_finger=0):
+    layout = [1,0,1,0,0,1,0,1]
+    rotation = [0,1,2,3,7,6,5,4]
+    if starting_finger < 4:
+        starting_index = starting_finger
+    else:
+        starting_index = 11 - starting_finger
+    rotation = rotation[starting_index:] + rotation[:starting_index]
+    return layout, global_rotation(layout, rotation, pattern)
+
+
+def dual_rotation(d_rotation, k_rotation, pattern):
+    length = len(d_rotation) + len(k_rotation)
+    if not all([(i in d_rotation or i in k_rotation) for i in range(length)]):
+        raise ValueError('invalid d and k rotations')
+    layout = [(i in k_rotation) for i in range(length)]
+    d_i = 0
+    k_i = 0
+    fingering = []
+    for note in pattern:
+        if note:
+            fingering.append(k_rotation[k_i])
+            k_i = (k_i + 1) % len(k_rotation)
+        else:
+            fingering.append(d_rotation[d_i])
+            d_i = (d_i + 1) % len(d_rotation)
+    return layout, fingering
+
+
+def handwise_dual_rotation(keys_per_hand, pattern, d_starting_finger=0, k_starting_finger=None):
+    n = keys_per_hand
+    if k_starting_finger is None:
+        k_starting_finger = 2 * n - 1
+    if d_starting_finger >= n:
+        raise ValueError('starting finger does not match layout color')
+    else:
+        d_rotation = [(d_starting_finger + i) % n for i in range(n)]
+    if k_starting_finger < n:
+        raise ValueError('starting finger does not match layout color')
+    else:
+        k_rotation = [n + (k_starting_finger - n - i) % n for i in range(n)]
+    return dual_rotation(d_rotation, k_rotation, pattern)
+
+
+def ddkk_dual_rotation(pattern, d_starting_finger=0, k_starting_finger=3):
+    return handwise_dual_rotation(2, pattern, d_starting_finger, k_starting_finger)
+
+
+def ddddkkkk_dual_rotation(pattern, d_starting_finger=0, k_starting_finger=7):
+    return handwise_dual_rotation(4, pattern, d_starting_finger, k_starting_finger)
+    
+
+four_key_styles = [
+    kddk_full_wrist_alt,
+    kddk_full_roll,
+    kddk_half_roll,
+    ddkk_dual_rotation
+]
+eight_key_styles = [
+    kdkddkdk_half_roll,
+    ddddkkkk_dual_rotation
+]
